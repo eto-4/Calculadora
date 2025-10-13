52467b709502e3be84e684aed481433797cb0854
Aleix <aleix.murt22@lacetania.cat>
2025-10-13 19:43:38 +0200

    Base de la calc

---
+++ assets/js/main.js
@@ -0,0 +1,51 @@
+
+// Layout
+const keyLayout = [
+    ['MR','M+','M-','%'],
+    ['7','8','9','/'],
+    ['4','5','6','*'],
+    ['1','2','3','-'],
+    ['0','.','=', '+']
+];
+
+
+// Funcionalitats
+const keys = [
+
+    // --- Funcions de memòria ---
+    {label: 'MC', value: 'mem-clear', type: 'func'},
+    {label: 'MR', value: 'mem-recall', type: 'func'},
+    {label: 'M+', value: 'mem-add', type: 'func'},
+    {label: 'M-', value: 'mem-sub', type: 'func'},
+
+    // --- Operadors bàsics ---
+    {label: '%', value: '%', type: 'op'},
+    {label: '÷', value: '/', type: 'op'},
+    {label: '×', value: '*', type: 'op'},
+    {label: '-', value: '-', type: 'op'},
+    {label: '+', value: '+', type: 'op'},
+
+    // --- Números ---
+    {label: '7', value: '7', type: 'num'},
+    {label: '8', value: '8', type: 'num'},
+    {label: '9', value: '9', type: 'num'},
+    {label: '4', value: '4', type: 'num'},
+    {label: '5', value: '5', type: 'num'},
+    {label: '6', value: '6', type: 'num'},
+    {label: '1', value: '1', type: 'num'},
+    {label: '2', value: '2', type: 'num'},
+    {label: '3', value: '3', type: 'num'},
+    {label: '0', value: '0', type: 'num'},
+
+    // --- Funcions Adicionals ---
+    {label: '+/-', value: 'neg', type: 'func'},
+    {label: '.', value: '.', type: 'func'},
+    {label: '=', value: 'result', type: 'func'},
+];
+
+// Functions
+
+// Creació de botons
+function crearBoto() {
+    
+}
